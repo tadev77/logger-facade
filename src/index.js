8cab892cd67b2cf9ccdb63720f78e09da20e458b
Tadeu Oliveira <tadeu.oliveira.e4@gmail.com>
2025-04-01 08:40:57 -0300

    doc: adiciona documentação para getDataDogInstance

--- src/index.js
+++ src/index.js
@@ -6,7 +6,25 @@ class LoggerFacade {
       datadog: DataDogProvider,
     };
   }
-
+/**
+ * returns a logger instance that uses datadog under the hood
+ * 
+ * @param {Object} options - Configuration object.
+ * @param {string} options.apiKey - API key for Datadog.
+ * @param {string} options.serviceName - Service name for Datadog.
+ * 
+ * @returns {Object} A instância configurada para envio de logs ao Datadog.
+ * 
+ * @example
+ * const loggerFacade = require('@tadev77/logger-facade');
+ * 
+ * const logger = loggerFacade.getDataDogInstance({
+ *   apiKey: 'my-api-key',
+ *   serviceName: 'my-service',
+ * });
+ * 
+ * logger.sendLog('info', { message: 'My first log!' });
+ */
   getDataDogInstance(options) {
     return new this.providers.datadog(options);
   }
